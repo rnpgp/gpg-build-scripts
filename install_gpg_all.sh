b8f67966758cd8a69e448956de69951df171306b
Sebastian Skałacki <skalee@gmail.com>
2018-06-13 21:46:37 +0200

    wip

--- install_gpg_all.sh
+++ install_gpg_all.sh
@@ -17,40 +17,40 @@ options="$@"
 
 case "${version}" in
 	"2.2")
-		./install_gpg_component.sh libgpg-error 1.31 "${options}"
-		./install_gpg_component.sh libgcrypt 1.8.2 "${options}"
-		./install_gpg_component.sh libassuan 2.5.1 "${options}"
-		./install_gpg_component.sh libksba 1.3.5 "${options}"
-		./install_gpg_component.sh npth 1.5 "${options}"
-		./install_gpg_component.sh pinentry 1.1.0 "${options}"
-		./install_gpg_component.sh gnupg 2.2.7 "${options}"
+		./install_gpg_component.sh libgpg-error 1.31 ${options}
+		./install_gpg_component.sh libgcrypt 1.8.2 ${options}
+		./install_gpg_component.sh libassuan 2.5.1 ${options}
+		./install_gpg_component.sh libksba 1.3.5 ${options}
+		./install_gpg_component.sh npth 1.5 ${options}
+		./install_gpg_component.sh pinentry 1.1.0 ${options}
+		./install_gpg_component.sh gnupg 2.2.7 ${options}
 		;;
 	"2.1")
-		./install_gpg_component.sh libgpg-error 1.27 "${options}"
-		./install_gpg_component.sh libgcrypt 1.7.6 "${options}"
-		./install_gpg_component.sh libassuan 2.4.3 "${options}"
-		./install_gpg_component.sh libksba 1.3.5 "${options}"
-		./install_gpg_component.sh npth 1.2 "${options}"
-		./install_gpg_component.sh pinentry 0.9.5 "${options}"
-		./install_gpg_component.sh gnupg 2.1.20 "${options}"
+		./install_gpg_component.sh libgpg-error 1.27 ${options}
+		./install_gpg_component.sh libgcrypt 1.7.6 ${options}
+		./install_gpg_component.sh libassuan 2.4.3 ${options}
+		./install_gpg_component.sh libksba 1.3.5 ${options}
+		./install_gpg_component.sh npth 1.2 ${options}
+		./install_gpg_component.sh pinentry 0.9.5 ${options}
+		./install_gpg_component.sh gnupg 2.1.20 ${options}
 		;;
 	"latest")
-		./install_gpg_component.sh libgpg-error latest "${options}"
-		./install_gpg_component.sh libgcrypt latest "${options}"
-		./install_gpg_component.sh libassuan latest "${options}"
-		./install_gpg_component.sh libksba latest "${options}"
-		./install_gpg_component.sh npth latest "${options}"
-		./install_gpg_component.sh pinentry latest "${options}"
-		./install_gpg_component.sh gnupg latest "${options}"
+		./install_gpg_component.sh libgpg-error latest ${options}
+		./install_gpg_component.sh libgcrypt latest ${options}
+		./install_gpg_component.sh libassuan latest ${options}
+		./install_gpg_component.sh libksba latest ${options}
+		./install_gpg_component.sh npth latest ${options}
+		./install_gpg_component.sh pinentry latest ${options}
+		./install_gpg_component.sh gnupg latest ${options}
 		;;
 	"master")
-		./install_gpg_component.sh libgpg-error master --git "${options}"
-		./install_gpg_component.sh libgcrypt master --git "${options}"
-		./install_gpg_component.sh libassuan master --git "${options}"
-		./install_gpg_component.sh libksba master --git "${options}"
-		./install_gpg_component.sh npth master --git "${options}"
-		./install_gpg_component.sh pinentry master --git "${options}"
-		./install_gpg_component.sh gnupg master --git "${options}"
+		./install_gpg_component.sh libgpg-error master --git ${options}
+		./install_gpg_component.sh libgcrypt master --git ${options}
+		./install_gpg_component.sh libassuan master --git ${options}
+		./install_gpg_component.sh libksba master --git ${options}
+		./install_gpg_component.sh npth master --git ${options}
+		./install_gpg_component.sh pinentry master --git ${options}
+		./install_gpg_component.sh gnupg master --git ${options}
 		;;
 esac
 
