274669d188f42c8bbae35917e4324463db4b7bc4
Sebastian Skałacki <skalee@gmail.com>
2018-06-24 04:49:23 +0200

    Improve GPG build scripts

--- install_gpg_all.sh
+++ install_gpg_all.sh
@@ -6,54 +6,54 @@
 # For v2.1: https://gist.github.com/mattrude/3883a3801613b048d45b
 #
 # USAGE:
-#   ./install_gpg_all <version> [<options>]
+#   ./install_gpg_all.sh <version> [<options>]
+#
 # EXAMPLE
-#   ./install_gpg_all 2.2
+#   ./install_gpg_all.sh 2.2
+
 set -e
 
-version="$1"
-shift
-options="$@"
-
-case "${version}" in
+case "$1" in
 	"2.2")
-		./install_gpg_component.sh libgpg-error 1.31 ${options}
-		./install_gpg_component.sh libgcrypt 1.8.2 ${options}
-		./install_gpg_component.sh libassuan 2.5.1 ${options}
-		./install_gpg_component.sh libksba 1.3.5 ${options}
-		./install_gpg_component.sh npth 1.5 ${options}
-		./install_gpg_component.sh pinentry 1.1.0 ${options}
-		./install_gpg_component.sh gnupg 2.2.7 ${options}
+		./install_gpg_component.sh --component libgpg-error --version 1.31 "${@:2}"
+		./install_gpg_component.sh --component libgcrypt --version 1.8.2 "${@:2}"
+		./install_gpg_component.sh --component libassuan --version 2.5.1 "${@:2}"
+		./install_gpg_component.sh --component libksba --version 1.3.5 "${@:2}"
+		./install_gpg_component.sh --component npth --version 1.5 "${@:2}"
+		./install_gpg_component.sh --component pinentry --version 1.1.0 "${@:2}"
+		./install_gpg_component.sh --component gnupg --version 2.2.7 "${@:2}"
 		;;
 	"2.1")
-		./install_gpg_component.sh libgpg-error 1.27 ${options}
-		./install_gpg_component.sh libgcrypt 1.7.6 ${options}
-		./install_gpg_component.sh libassuan 2.4.3 ${options}
-		./install_gpg_component.sh libksba 1.3.5 ${options}
-		./install_gpg_component.sh npth 1.2 ${options}
-		./install_gpg_component.sh pinentry 0.9.5 ${options}
-		./install_gpg_component.sh gnupg 2.1.20 ${options}
+		./install_gpg_component.sh --component libgpg-error --version 1.27 "${@:2}"
+		./install_gpg_component.sh --component libgcrypt --version 1.7.6 "${@:2}"
+		./install_gpg_component.sh --component libassuan --version 2.4.3 "${@:2}"
+		./install_gpg_component.sh --component libksba --version 1.3.5 "${@:2}"
+		./install_gpg_component.sh --component npth --version 1.2 "${@:2}"
+		./install_gpg_component.sh --component pinentry --version 0.9.5 "${@:2}"
+		./install_gpg_component.sh --component gnupg --version 2.1.20 "${@:2}"
 		;;
 	"latest")
-		./install_gpg_component.sh libgpg-error latest ${options}
-		./install_gpg_component.sh libgcrypt latest ${options}
-		./install_gpg_component.sh libassuan latest ${options}
-		./install_gpg_component.sh libksba latest ${options}
-		./install_gpg_component.sh npth latest ${options}
-		./install_gpg_component.sh pinentry latest ${options}
-		./install_gpg_component.sh gnupg latest ${options}
+		./install_gpg_component.sh --component libgpg-error --version latest "${@:2}"
+		./install_gpg_component.sh --component libgcrypt --version latest "${@:2}"
+		./install_gpg_component.sh --component libassuan --version latest "${@:2}"
+		./install_gpg_component.sh --component libksba --version latest "${@:2}"
+		./install_gpg_component.sh --component npth --version latest "${@:2}"
+		./install_gpg_component.sh --component pinentry --version latest "${@:2}"
+		./install_gpg_component.sh --component gnupg --version latest "${@:2}"
 		;;
 	"master")
-		./install_gpg_component.sh libgpg-error master --git ${options}
-		./install_gpg_component.sh libgcrypt master --git ${options}
-		./install_gpg_component.sh libassuan master --git ${options}
-		./install_gpg_component.sh libksba master --git ${options}
-		./install_gpg_component.sh npth master --git ${options}
-		./install_gpg_component.sh pinentry master --git ${options}
-		./install_gpg_component.sh gnupg master --git ${options}
+		./install_gpg_component.sh --component libgpg-error --version master --git "${@:2}"
+		./install_gpg_component.sh --component libgcrypt --version master --git "${@:2}"
+		./install_gpg_component.sh --component libassuan --version master --git "${@:2}"
+		./install_gpg_component.sh --component libksba --version master --git "${@:2}"
+		./install_gpg_component.sh --component npth --version master --git "${@:2}"
+		./install_gpg_component.sh --component pinentry --version master --git "${@:2}"
+		./install_gpg_component.sh --component gnupg --version master --git "${@:2}"
 		;;
 esac
 
-echo "===="
-echo "DONE"
-echo "===="
+cat <<DONE
+----------------
+|     DONE     |
+----------------
+DONE
