51a89eedf6746a2fdb8aa47996711767495a6446
Sebastian Skałacki <skalee@gmail.com>
2018-11-05 16:15:16 +0100

    Write usage examples, and test them in Travis
    Every example is tested in Travis CI.  They all together comprise a test suite
    for `gpg-build-scripts`.  Every example is arranged as follows: it begins
    with a variable definitions block, which is followed with actual installation
    commands, and ends with a sequence of assertions which confirm installation
    correctness.  Every example includes comments.

---
+++ examples/all_latest.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e # Early exit if any command returns non-zero status code
+set -v # Print executed lines
+
+# This example shows installation of the latest version of GnuPG
+# (currently 2.2).
+
+###############
+#    SETUP    #
+###############
+
+# The --sudo option is typically needed when installing to standard locations.
+# Note that `sudo ./install_gpg_all …` is not the same—it would compile as
+# root (not recommended), and won't trigger post-install steps (including
+# ldconfig).
+./install_gpg_all.sh latest --sudo
+
+###############
+#    TESTS    #
+###############
+
+# Assert path to executable…
+[[ $(which gpg) == "/usr/local/bin/gpg" ]]
+
+# Assert that executable actually works…
+gpg --version
+
+# Assert executable version…
+gpg --version | head -n 1 | cut -d" " -f 3 | grep -xE "2\.2\.[0-9]+"
---
+++ examples/build_dir.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -e # Early exit if any command returns non-zero status code
+set -v # Print executed lines
+
+# This example shows how to use the --build-dir option.
+#
+# By default, the install script creates a temporary directory in which
+# the whole build happens.  However, sometimes you may want to use a specific
+# directory instead.  Typical reasons are caching the source code, or keeping
+# the build which has failed for inspection.
+
+###############
+#    SETUP    #
+###############
+
+BUILD_DIR="${TRAVIS_BUILD_DIR}/b"
+
+mkdir -p ${BUILD_DIR}
+
+# The --sudo option is typically needed when installing to standard locations.
+# Note that `sudo ./install_gpg_all …` is not the same—it would compile as
+# root (not recommended), and won't trigger post-install steps (including
+# ldconfig).
+./install_gpg_all.sh 2.2 --sudo --build-dir ${BUILD_DIR}
+
+###############
+#    TESTS    #
+###############
+
+# Assert path to executable…
+[[ $(which gpg) == "/usr/local/bin/gpg" ]]
+
+# Assert that executable actually works…
+gpg --version
+
+# Assert executable version…
+gpg --version | head -n 1 | cut -d" " -f 3 | grep -xE "2\.2\.[0-9]+"
+
+# Assert that ${BUILD_DIR} is now full of files (cause build had happened
+# there)…
+pushd ${BUILD_DIR}
+ls -d libgpg-error-*
+ls -d gnupg-*
+[[ -f "$(ls | grep "libgpg-error-")/Makefile" ]]
+[[ -f "$(ls | grep "gnupg-")/Makefile" ]]
+popd
