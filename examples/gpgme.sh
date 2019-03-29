04517dcd73abb277d4207bc31130eb2f91dac70c
Sebastian Skałacki <skalee@users.noreply.github.com>
2019-03-29 01:40:57 +0100

    Explain how to install GPGME (#35)
    Add a code example and write a few words in README.

---
+++ examples/gpgme.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e # Early exit if any command returns non-zero status code
+set -v # Print executed lines
+
+# This example shows how to install GnuPG Made Easy (GPGME) API.
+
+###############
+#    SETUP    #
+###############
+
+# The --sudo option is typically needed when installing to standard locations.
+# Note that `sudo ./install_gpg_all …` is not the same—it would compile as
+# root (not recommended), and won't trigger post-install steps (including
+# ldconfig).
+./install_gpg_all.sh --suite-version latest --sudo
+./install_gpg_component.sh --component-name gpgme --component-version latest --sudo
+
+###############
+#    TESTS    #
+###############
+
+# Assert path to executables…
+[[ $(which gpg) == "/usr/local/bin/gpg" ]]
+[[ $(which gpgme-config) == "/usr/local/bin/gpgme-config" ]]
+
+# Assert that executables actually works…
+gpg --version
+gpgme-config --version
+
+# Assert executable version…
+gpgme-config --version | head -n 1 | cut -d" " -f 3 | grep -xE "1\.[0-9]+\.[0-9]+"
+
+# Assert GPGME prefix…
+[[ $(gpgme-config --prefix) == "/usr/local" ]]
+
+# Assert the presence of dynamic library…
+[[ -f /usr/local/lib/libgpgme.so ]]
